646b4ce730d1adc1e842c4d7d5b135dac574bf74
Samira Ahmed <B00156786@mytudublin.ie>
2024-11-22 16:06:51 +0000

    first commit

--- App.js
+++ App.js
@@ -1,5 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { View, Text, TextInput, Button,  StyleSheet } from 'react-native';
+import {Picker} from '@react-native-picker/picker';
+import appstyles from './assets/appstyles.js'; // Import styles
+//import ColorPickerScreen from './components/colourpickerscreen';
+import RegistrationScreen from './components/registerscreen'
+//import Homescreen from './components/Homescreen'
+//import RequestScreen from './components/Requestscreen'
+//import MessagesScreen from './components/MessagesScreen'
+//import ProfileScreen from './components/ProfileScreen'
 
 export default function App() {
   return (
